b3492606b29f7d398f6aa5c1f9da09496226145a
Muktarali-uulu-Dastanbek <muktaraliuuludastan@gmail.com>
2024-08-25 21:18:59 +0600

    removed DefaultTabController Widget

--- lib/src/feature/main/widgets/page/main_page.dart
+++ lib/src/feature/main/widgets/page/main_page.dart
@@ -29,187 +29,184 @@ class _MainPageState extends State<MainPage>
     if (newValue != null) {
       setState(() {
         selectedValue = newValue;
-        _tabController.index = tabIndex; // Устанавливаем активную вкладку
+        _tabController.index = tabIndex;
       });
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 7,
-      child: Scaffold(
-        body: Column(
-          children: [
-            Container(
-              height: 50,
-              color: Colors.blue.shade900,
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 50,
+            color: Colors.blue.shade900,
+          ),
+          SizedBox(height: 5),
+          TabBar(
+            indicatorWeight: 1,
+            controller: _tabController,
+            overlayColor: MaterialStateProperty.resolveWith<Color?>(
+              (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return Colors.transparent;
+                }
+                return null;
+              },
             ),
-            SizedBox(height: 5),
-            TabBar(
-              indicatorWeight: 1,
-              controller: _tabController,
-              overlayColor: MaterialStateProperty.resolveWith<Color?>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.hovered)) {
-                    return Colors.transparent;
-                  }
-                  return null;
-                },
+            padding: EdgeInsets.zero,
+            indicatorPadding: EdgeInsets.zero,
+            labelPadding: EdgeInsets.symmetric(horizontal: 2),
+            tabAlignment: TabAlignment.center,
+            isScrollable: true,
+            tabs: [
+              Tab(
+                child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 90, maxWidth: 200),
+                  child: DropdownMenu<String>(
+                    expandedInsets: EdgeInsets.zero,
+                    width: 150,
+                    hintText: 'Парламент',
+                    initialSelection: selectedValue,
+                    onSelected: (String? newValue) {
+                      _onDropdownValueChanged(newValue, 0);
+                    },
+                    dropdownMenuEntries: TabBarDropDownItems.parlamentDropDown
+                        .map(
+                          (element) => DropdownMenuEntry(
+                            value: element,
+                            label: element,
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
               ),
-              padding: EdgeInsets.zero,
-              indicatorPadding: EdgeInsets.zero,
-              labelPadding: EdgeInsets.symmetric(horizontal: 2),
-              tabAlignment: TabAlignment.center,
-              isScrollable: true,
-              tabs: [
-                Tab(
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxHeight: 90, maxWidth: 200),
-                    child: DropdownMenu<String>(
-                      expandedInsets: EdgeInsets.zero,
-                      width: 150,
-                      hintText: 'Парламент',
-                      initialSelection: selectedValue,
-                      onSelected: (String? newValue) {
-                        _onDropdownValueChanged(newValue, 0);
-                      },
-                      dropdownMenuEntries: TabBarDropDownItems.parlamentDropDown
-                          .map(
-                            (element) => DropdownMenuEntry(
-                              value: element,
-                              label: element,
-                            ),
-                          )
-                          .toList(),
-                    ),
+              Tab(
+                child: Container(
+                  height: 90,
+                  width: 100,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black45),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Text('Торага'),
+                ),
+              ),
+              Tab(
+                child: Container(
+                  height: 90,
+                  width: 200,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black45),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Text('Заместители Торага'),
+                ),
+              ),
+              Tab(
+                child: Container(
+                  height: 90,
+                  width: 100,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    border: Border.all(width: 1, color: Colors.black45),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  child: Text('Депутаты'),
+                ),
+              ),
+              Tab(
+                child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 90, maxWidth: 200),
+                  child: DropdownMenu<String>(
+                    expandedInsets: EdgeInsets.zero,
+                    // width: 200,
+                    hintText: 'Деятельность ЖК',
+                    initialSelection: selectedValue,
+                    onSelected: (String? newValue) {
+                      _onDropdownValueChanged(newValue, 4);
+                    },
+                    dropdownMenuEntries:
+                        TabBarDropDownItems.deyatelnostJKDropDown
+                            .map(
+                              (element) => DropdownMenuEntry(
+                                value: element,
+                                label: element,
+                              ),
+                            )
+                            .toList(),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 90,
-                    width: 100,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black45),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Text('Торага'),
+              ),
+              Tab(
+                child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 90, maxWidth: 200),
+                  child: DropdownMenu<String>(
+                    expandedInsets: EdgeInsets.zero,
+                    hintText: 'Структура ЖК',
+                    initialSelection: selectedValue,
+                    onSelected: (String? newValue) {
+                      _onDropdownValueChanged(newValue, 5);
+                    },
+                    dropdownMenuEntries: TabBarDropDownItems.structuraJKDropDown
+                        .map(
+                          (element) => DropdownMenuEntry(
+                            value: element,
+                            label: element,
+                          ),
+                        )
+                        .toList(),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 90,
-                    width: 200,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black45),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Text('Заместители Торага'),
+              ),
+              Tab(
+                child: ConstrainedBox(
+                  constraints:
+                      const BoxConstraints(maxHeight: 90, maxWidth: 150),
+                  child: DropdownMenu<String>(
+                    expandedInsets: EdgeInsets.zero,
+                    width: 150,
+                    hintText: 'Медиа',
+                    initialSelection: selectedValue,
+                    onSelected: (String? newValue) {
+                      _onDropdownValueChanged(newValue, 6);
+                    },
+                    dropdownMenuEntries: TabBarDropDownItems.mediaDropDown
+                        .map(
+                          (element) => DropdownMenuEntry(
+                            value: element,
+                            label: element,
+                          ),
+                        )
+                        .toList(),
                   ),
                 ),
-                Tab(
-                  child: Container(
-                    height: 90,
-                    width: 100,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                      border: Border.all(width: 1, color: Colors.black45),
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                    child: Text('Депутаты'),
-                  ),
-                ),
-                Tab(
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxHeight: 90, maxWidth: 200),
-                    child: DropdownMenu<String>(
-                      expandedInsets: EdgeInsets.zero,
-                      // width: 200,
-                      hintText: 'Деятельность ЖК',
-                      initialSelection: selectedValue,
-                      onSelected: (String? newValue) {
-                        _onDropdownValueChanged(newValue, 4);
-                      },
-                      dropdownMenuEntries:
-                          TabBarDropDownItems.deyatelnostJKDropDown
-                              .map(
-                                (element) => DropdownMenuEntry(
-                                  value: element,
-                                  label: element,
-                                ),
-                              )
-                              .toList(),
-                    ),
-                  ),
-                ),
-                Tab(
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxHeight: 90, maxWidth: 200),
-                    child: DropdownMenu<String>(
-                      expandedInsets: EdgeInsets.zero,
-                      hintText: 'Структура ЖК',
-                      initialSelection: selectedValue,
-                      onSelected: (String? newValue) {
-                        _onDropdownValueChanged(newValue, 5);
-                      },
-                      dropdownMenuEntries:
-                          TabBarDropDownItems.structuraJKDropDown
-                              .map(
-                                (element) => DropdownMenuEntry(
-                                  value: element,
-                                  label: element,
-                                ),
-                              )
-                              .toList(),
-                    ),
-                  ),
-                ),
-                Tab(
-                  child: ConstrainedBox(
-                    constraints:
-                        const BoxConstraints(maxHeight: 90, maxWidth: 150),
-                    child: DropdownMenu<String>(
-                      expandedInsets: EdgeInsets.zero,
-                      width: 150,
-                      hintText: 'Медиа',
-                      initialSelection: selectedValue,
-                      onSelected: (String? newValue) {
-                        _onDropdownValueChanged(newValue, 6);
-                      },
-                      dropdownMenuEntries: TabBarDropDownItems.mediaDropDown
-                          .map(
-                            (element) => DropdownMenuEntry(
-                              value: element,
-                              label: element,
-                            ),
-                          )
-                          .toList(),
-                    ),
-                  ),
-                ),
+              ),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              physics: NeverScrollableScrollPhysics(),
+              children: [
+                Center(child: Text("Car Tab")),
+                Center(child: Text("Transit Tab")),
+                Center(child: Text("Bike Tab")),
+                Center(child: Text("Bike Tab")),
+                Center(child: Text("Bike Tab")),
+                Center(child: Text("Bike Tab")),
+                Center(child: Text("Bike Tab")),
               ],
             ),
-            Expanded(
-              child: TabBarView(
-                physics: NeverScrollableScrollPhysics(),
-                children: [
-                  Center(child: Text("Car Tab")),
-                  Center(child: Text("Transit Tab")),
-                  Center(child: Text("Bike Tab")),
-                  Center(child: Text("Bike Tab")),
-                  Center(child: Text("Bike Tab")),
-                  Center(child: Text("Bike Tab")),
-                  Center(child: Text("Bike Tab")),
-                ],
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
