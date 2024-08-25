9eb29a9d0e2f607f0fd9c90bbdfc269644fed918
Muktarali-uulu-Dastanbek <muktaraliuuludastan@gmail.com>
2024-08-25 23:37:23 +0600

    detailed TabBarPage and added ru&ky.arb

--- lib/src/common/app_router/app_router.dart
+++ lib/src/common/app_router/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:kenesh_kg/src/feature/main/widgets/page/main_page.dart';
+import 'package:kenesh_kg/src/feature/tab_bar/widgets/page/tab_bar.dart';
 
 import 'app_routes_names.dart';
 import 'error_page.dart';
@@ -14,7 +14,7 @@ final GoRouter router = GoRouter(
     GoRoute(
       name: AppRoutesNames.mainPage,
       path: "/",
-      builder: (context, state) => const MainPage(),
+      builder: (context, state) => const TabBarPage(),
     ),
   ],
 );
---
+++ lib/src/feature/tab_bar/widgets/dropdown_tab_item.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class DropdownTabItem extends StatelessWidget {
+  final double maxWidth;
+  final String hintText;
+  final List<DropdownMenuEntry<String>> dropdownMenuEntries;
+  final String? initialSelection;
+  final Function(String?)? onSelected;
+
+  const DropdownTabItem({
+    Key? key,
+    required this.maxWidth,
+    required this.hintText,
+    required this.dropdownMenuEntries,
+    this.initialSelection,
+    this.onSelected,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: maxWidth),
+        child: DropdownMenu<String>(
+          expandedInsets: EdgeInsets.zero,
+          hintText: hintText,
+          initialSelection: initialSelection,
+          onSelected: onSelected,
+          dropdownMenuEntries: dropdownMenuEntries,
+        ),
+      ),
+    );
+  }
+}
